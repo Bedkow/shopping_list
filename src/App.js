ad984c89d05e98e994a87742d117a9af7bdbc801
Bedkow <bednarekdaniel@o2.pl>
2021-08-26 21:37:02 +0200

    state management ex

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+	const handleClickAddNewItem = () => {
+		setInputItemName("new name");
+		setInputItemNumber(4);
+	};
+
+	// [initial value, function used to change the value by passing smth to it later]
+	const [inputItemName, setInputItemName] = useState("xxx");
+	const [inputItemNumber, setInputItemNumber] = useState(6);
+
 	return (
 		<div>
 			<Header />
+
+			<div className="item">
+				<input
+					type="text"
+					className="name-input"
+					placeholder="Item's name..."
+				></input>
+				<input
+					type="number"
+					className="number-input"
+					placeholder="Quantity..."
+				></input>
+				<div className="btn-container">
+					<button className="float-right-btn" onClick={handleClickAddNewItem}>
+						+
+					</button>
+				</div>
+			</div>
+
+			<div className="new-item">
+				<span>
+					Item name: {inputItemName} Number: {inputItemNumber}
+				</span>
+			</div>
 
 			<Footer />
 		</div>
