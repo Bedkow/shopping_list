dc6aa6fa4a6d3e7a3c3cf1cf7d715c012fce71ae
Bedkow <bednarekdaniel@o2.pl>
2021-08-31 22:14:45 +0200

    basic functionality added and working

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
 	// [initial value, function used to change the value by passing smth to it later]
 	const [inputItemName, setInputItemName] = useState("");
 	const [inputItemNumber, setInputItemNumber] = useState("");
-	const [arrayOfItems, setArrayOfItems] = useState([
-		{ itemName: "", quantity: "", id: "" },
-	]);
+	const [arrayOfItems, setArrayOfItems] = useState([]);
 
 	const handleChangeNewName = (event) => {
 		setInputItemName(event.target.value);
@@ -20,15 +18,25 @@ function App() {
 	};
 
 	const handleAddNewItem = () => {
-		const newItem = {
-			itemName: inputItemName,
-			quantity: inputItemNumber,
-			id: Math.random(),
-		};
+		if (!inputItemName) {
+			alert("Please enter the item's name.");
+		} else {
+			const newItem = {
+				itemName: inputItemName,
+				quantity: inputItemNumber,
+				id: Math.random(),
+			};
 
-		const newArrayOfItems = [...arrayOfItems, newItem];
+			const newArrayOfItems = [...arrayOfItems, newItem];
 
-		setArrayOfItems(newArrayOfItems);
+			setArrayOfItems(newArrayOfItems);
+			setInputItemNumber("");
+			setInputItemName("");
+		}
+	};
+
+	const handleDeleteAllItems = () => {
+		setArrayOfItems([]);
 		setInputItemNumber("");
 		setInputItemName("");
 	};
@@ -44,6 +52,7 @@ function App() {
 					className="name-input"
 					placeholder="Item's name..."
 					onChange={handleChangeNewName}
+					maxLength="50"
 				></input>
 				<input
 					value={inputItemNumber}
@@ -51,26 +60,40 @@ function App() {
 					className="number-input"
 					placeholder="Quantity..."
 					onChange={handleChangeNewNumber}
+					min="1"
+					max="1000000000"
 				></input>
 				<div className="btn-container">
 					<button className="float-right-btn" onClick={handleAddNewItem}>
 						+
 					</button>
 				</div>
+
+				{/* delete all items */}
+				<div className="btn-container">
+					<button className="float-right-btn" onClick={handleDeleteAllItems}>
+						X
+					</button>
+				</div>
 			</div>
 
-			<div className="new-item">
+			<div className="new-item-preview">
 				<span>
-					Item name: {inputItemName} Number: {inputItemNumber}
+					Item's name: {inputItemName} Quantity: {inputItemNumber}
 				</span>
 			</div>
 
-			<ul>
+			<ul className="list-of-items">
 				{arrayOfItems.map((item) => (
-					<li key={item.itemName + item.quantity + item.id}>
-						<span>{item.itemName}--- </span>
-						<span>{item.quantity}--- </span>
-						<span>{item.id} </span>
+					<li
+						className="new-item"
+						key={item.itemName + item.quantity + item.id}
+					>
+						<span>
+							<input type="checkbox" />
+						</span>
+						<span>{item.itemName} </span>
+						<span>{item.quantity} </span>
 					</li>
 				))}
 			</ul>
